632816635962cd95306c56d75b35065e7f2a44b6
rafaelfdeoliveira <raf100guy@gmail.com>
2021-12-06 21:38:03 -0300

    Exercicio Turno

--- src/com/rafael/Main.java
+++ src/com/rafael/Main.java
@@ -1,11 +1,13 @@
 package com.rafael;
 
-import com.rafael.aula2.AreaQuadrado;
-import com.rafael.aula2.AreaTriangulo;
-import com.rafael.aula2.Avatar;
-import com.rafael.aula2.MediaNotas;
+//import com.rafael.aula2.AreaQuadrado;
+//import com.rafael.aula2.AreaTriangulo;
+//import com.rafael.aula2.Avatar;
+//import com.rafael.aula2.MediaNotas;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -51,13 +53,67 @@ public class Main {
 //        System.out.println("Informe um valor para o byte: ");
 //        byte b = sc.nextByte();
 //        System.out.println("O valor informado foi " + b);
-        final float dolarToReal = 5.65f;
-        final float euroToReal = 6.39f;
-        System.out.println("Insira um valor em reais: ");
-        Scanner sc = new Scanner(System.in);
-        float reais = sc.nextFloat();
-        System.out.printf("Você possui %.2f dólares (USD)", reais/dolarToReal);
-        System.out.printf("Você possui %.2f euros (EUR)", reais/euroToReal);
+//        final float dolarToReal = 5.65f;
+//        final float euroToReal = 6.39f;
+//        System.out.println("Insira um valor em reais: ");
+//        Scanner sc = new Scanner(System.in);
+//        float reais = sc.nextFloat();
+//        System.out.printf(Locale.US, "Você possui %.2f dólares (USD)", reais/dolarToReal);
+//        System.out.printf(Locale.US, "Você possui %.2f euros (EUR)", reais/euroToReal);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Cálculo das 4 operações básicas com dois números inteiros fornecidos.");
+//        System.out.println("Insira o primeiro número:");
+//        int firstNumber = sc.nextInt();
+//        System.out.println("Insira o segundo número:");
+//        int secondNumber = sc.nextInt();
+//        int firstNumber = Integer.parseInt(args[0]);
+//        int secondNumber = Integer.parseInt(args[1]);
+//        System.out.printf("Soma: %d + %d = %d%n", firstNumber, secondNumber, firstNumber + secondNumber);
+//        System.out.printf("Subtração: %d - %d = %d%n", firstNumber, secondNumber, firstNumber - secondNumber);
+//        System.out.printf("Multiplicação: %d X %d = %d%n", firstNumber, secondNumber, firstNumber * secondNumber);
+//        System.out.printf("Divisão: %d / %d = %.2f%n", firstNumber, secondNumber, (float) firstNumber / secondNumber);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Insira um número de 1 a 10 para ver sua tabuada:");
+//        int number = sc.nextInt();
+//        System.out.printf("Tabuada do %d:%n", number);
+//        for (int i = 1; i < 11; i++) {
+//            System.out.printf("%d X %d = %d%n", number, i, number * i);
+//        }
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Insira o valor da temperatura em graus Celsius:");
+//        float celsius = sc.nextFloat();
+//        System.out.printf("%.1fºC é igual a %.1fºF%n", celsius, celsiusToFarenheit(celsius));
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Informe a cotação do dólar atual:");
+//        final float realToDolar = sc.nextFloat();
+//        System.out.println("Informe um valor em Reais:");
+//        final float reais = sc.nextFloat();
+//        final float dolars = reais / realToDolar;
+//        System.out.printf("R$%.2f equivalem a $%.2f%n", reais, dolars);
+//        final float inputReais = sc.nextFloat();
+//        final String dolars = DecimalFormat.getCurrencyInstance(Locale.US).format(inputReais / realToDolar);
+//        final String reais = DecimalFormat.getCurrencyInstance(Locale.forLanguageTag("pt-br")).format(inputReais);
+//        System.out.printf("%s equivalem a %s%n", reais, dolars);
+//        Scanner sc = new Scanner(System.in);
+//        final float inchToCentimeters = 2.54f;
+//        System.out.println("Informe o valor em polegadas:");
+//        final float inches = sc.nextFloat();
+//        final float centimeters = inches * inchToCentimeters;
+//        System.out.printf("%.2f polegadas equivalem a %.2f centímetros%n", inches, centimeters);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Informe a idade:");
+//        int idade = sc.nextInt();
+//        if (idade >= 16 && idade < 18 || idade > 70) {
+//            System.out.println("Se voto é opcional");
+//        } else if (idade < 16) {
+//            System.out.println("Não pode votar");
+//        } else {
+//            System.out.println("Seu voto é obrigatório");
+//        }
+    }
+
+    private static float celsiusToFarenheit (float celsius) {
+        return (float) (celsius * 1.8 + 32);
     }
 }
 
---
+++ src/com/rafael/Turno.java
@@ -0,0 +1,24 @@
+package com.rafael;
+
+import java.util.Scanner;
+
+public class Turno {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Em que turno voce estuda? (Insira M para matutino, V para vespertino ou N para noturno)");
+        String turno = sc.next();
+        switch (turno) {
+            case "M":
+                System.out.println("Bom dia!");
+                break;
+            case "V":
+                System.out.println("Boa tarde!");
+                break;
+            case "N":
+                System.out.println("Boa noite!");
+                break;
+            default:
+                System.out.println("Valor invalido");
+        }
+    }
+}
